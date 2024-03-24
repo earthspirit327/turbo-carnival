79fb8f1cc79693686f589ec86d6f2df1b78a57e1
earthspirit327 <137216979+earthspirit327@users.noreply.github.com>
2024-03-24 12:44:31 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-8aed349b-538d-46b6-ba9d-7e8c384af346/)
 
 
 echo "$curl_output" >> Update.tsx
